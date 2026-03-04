10c603eb5c46c359d635f994ce5704cc2a4af20d
salam-essa <salamessamsm@gmail.com>
2026-03-04 14:47:28 +0100

    Update script.js

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function() {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('show');
-                observer.unobserve(entry.target);
+            
             }
         });
     }, { threshold: 0.1, rootMargin: '0px 0px -50px 0px' });
