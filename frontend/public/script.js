dc80f3ab2b2a29942901e91b0d806f3a8a22382c
salam-essa <salamessamsm@gmail.com>
2026-03-07 01:00:05 +0100

    Update script.js

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -105,3 +105,12 @@ items[i].classList.add("active");
 }
 
 }
+document.querySelectorAll(".faq-question").forEach(btn=>{
+btn.addEventListener("click",()=>{
+
+const item = btn.parentElement;
+
+item.classList.toggle("active");
+
+});
+});
