0216d1eaf283f35553dd75706da04cbffb7540fb
salam-essa <salamessamsm@gmail.com>
2026-03-18 00:43:21 +0100

    Update bakend

--- frontend/public/login.js
+++ frontend/public/login.js
@@ -16,7 +16,7 @@ form.addEventListener("submit", async (e) => {
 
   try {
 
-    const response = await fetch("http://localhost:3000/api/login", {
+    const response = await fetch("http:/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
