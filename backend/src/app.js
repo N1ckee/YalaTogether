75fd548b14666d58f65a83645ea52dbaa1a8465f
salam-essa <salamessamsm@gmail.com>
2026-03-18 02:30:31 +0100

    Update koppla

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,10 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+//auth
+const authRoutes = require('../../frontend/api/auth');
+app.use('/auth', authRoutes);
+
 // Static frontend
 app.use(express.static(path.join(__dirname, '../../frontend/public')));
 
@@ -19,12 +23,29 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../../frontend/public/index.html'));
 });
 
+let rides = [];
+
+app.get("/api/rides", (req, res) => {
+  res.json(rides);
+});
+
+app.post("/api/rides", (req, res) => {
+  const newRide = {
+    id: Date.now(),
+    from: req.body.from,
+    to: req.body.to,
+    time: req.body.time,
+    seats: req.body.seats,
+    driver: "testUser"
+  };
+
+  rides.push(newRide);
+  res.json(newRide);
+});
+
 // 404 handler
 app.use((req, res) => {
   res.status(404).json({ error: 'Not found' });
 });
 
 module.exports = app;
-
-
-
