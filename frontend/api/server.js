acf89632cf37d65a6fe67a1aa7b3f0739a6daac3
N1ckee <nicke.hylander@gmail.com>
2026-03-18 19:15:56 +0100

    anothr "fix"

--- frontend/api/server.js
+++ frontend/api/server.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const path = require('path');
-const authRoutes = require('./auth');
+import express from "express";
+import path from "path";
+import authRoutes from "./auth.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/auth', authRoutes);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/auth", authRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-module.exports = (req, res) => {
-  console.log('API route hit');
+export default (req, res) => {
+  console.log("API route hit");
   app(req, res);
 };
