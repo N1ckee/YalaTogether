5582d4a464f74fdc9febba4f4012132948fc6a80
N1ckee <nicke.hylander@gmail.com>
2026-03-18 18:13:05 +0100

    routing fix

--- frontend/api/server.js
+++ frontend/api/server.js
@@ -8,12 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/public', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
+module.exports = (req, res) => {
+  console.log('API route hit');
+  app(req, res);
+};
