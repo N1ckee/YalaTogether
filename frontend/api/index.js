43349b7bd06d9609f7ea7292ac4d3872966ba7ae
N1ckee <nicke.hylander@gmail.com>
2026-03-18 20:55:16 +0100

    please work

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,26 +1,15 @@
 import express from "express";
-import path from "path";
 import authRoutes from "./auth.js";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// app.use(express.static(path.join(__dirname, "public")));
-app.use("/api/auth", authRoutes);
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/test", (req, res) => {
-  res.json({ ok: true, message: "main app works" });
+  res.json({ ok: true });
 });
+
+app.use("/api/auth", authRoutes);
 
 export default app;
